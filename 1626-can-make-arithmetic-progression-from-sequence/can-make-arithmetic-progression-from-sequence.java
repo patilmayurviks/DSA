655afbf6044a4d9d408c6d1dad91c2f310f48505
patilmayurviks <patilmayurvikas@gmail.com>
2026-09-07 00:29:22 +0530

    Time: 5 ms (83.85%) | Memory: 44.5 MB (10.89%) - LeetSync

---
+++ 1626-can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int same= arr[1]-arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]-arr[i-1]!=same){
+                return false;
+
+            }
+        }
+        return true;
+        
+    }
+}
